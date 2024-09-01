e68ca61531726da4cab09b38d60214a6a1b4dd76
flavius <flaviuscojocaru19@gmail.com>
2024-09-01 11:44:07 +0300

    working on clerk auth

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import SignUpFormProvider from "@/components/forms/form-provider";
+import React from "react";
+
+type Props = {};
+
+const Sign = (props: Props) => {
+  return (
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignUpFormProvider></SignUpFormProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Sign;
---
+++ src/context/use-auth-context.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+type initialValuesProps = {
+  currentStep: number;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const initialValues: initialValuesProps = {
+  currentStep: 1,
+  setCurrentStep: () => undefined,
+};
+
+const authContext = React.createContext(initialValues);
+
+const { Provider } = authContext;
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [currentStep, setCurrentStep] = useState<number>(
+    initialValues.currentStep
+  );
+
+  const values = {
+    currentStep,
+    setCurrentStep,
+  };
+
+  return <Provider value={values}>{children}</Provider>;
+};
+
+export const useAuthContextHook = () => {
+  const state = React.useContext(authContext);
+  return state;
+};
